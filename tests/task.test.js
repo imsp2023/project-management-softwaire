3f6ef83aa29b5db721a7132b0a9bbb93ae2dffc1
sogan carmen <sogancarmen1@gmail.com>
2023-12-16 21:03:10 +0100

    fichier de tests

--- tests/task.test.js
+++ tests/task.test.js
@@ -17,8 +17,6 @@ QUnit.module("Task", () => {
       }, new Error("the task parameter should be a Task object"));
     });
 
-    //On a une dependance cyclique à gerer
-    //Définir les differents de dependance cyclique
     test("throws an exception when the parameter task is same as task depends", (assert) => {
       let tk = new Task({});
       assert.throws(() => {
@@ -26,17 +24,11 @@ QUnit.module("Task", () => {
       }, new Error("the task parameter shouldn't same as task depends"));
     });
 
-    //tk1.dependOn(tk2, "DD")
-    //tk2.dependOn(tk3, "DD")
-    //tk3.dependOn(tk1, "DD")
-
-    //Choisir une dependance par défaut (DD) plutot que de lever une exception
-    test("throws an exception when the parameter dependanceType isn't specified", (assert) => {
+    test("set DD dependance by default when the parameter dependanceType isn't specified", (assert) => {
       let tk = new Task({});
       let tk1 = new Task({});
-      assert.throws(() => {
-        tk.dependsOn(tk1);
-      }, new Error("parameter dependanceType is required"));
+      tk.dependsOn(tk1);
+      assert.true(tk.getDependances()[0].typeOfDependance == "DD");
     });
 
     test("throws an exception when the parameter dependanceType isn't FF or DD or FD", (assert) => {
@@ -45,14 +37,6 @@ QUnit.module("Task", () => {
       assert.throws(() => {
         tk.dependsOn(tk1, "ls");
       }, new Error("parameter dependanceType should be DD or FF or FD"));
-    });
-
-    test("throws an exception when the parameter params isn't a string", (assert) => {
-      let tk = new Task({ startDate: new Date("2020-12-01") });
-      let tk1 = new Task({ startDate: new Date("2020-12-01") });
-      assert.throws(() => {
-        tk.dependsOn(tk1, "DD", 1);
-      }, new Error("parameter params should be a string"));
     });
 
     test("with invalid startDate when i apply DD dependancie, startDate should be change", (assert) => {
@@ -76,34 +60,28 @@ QUnit.module("Task", () => {
       assert.true(tk1.getStartDate() >= tk.getDueDate());
     });
 
-    //Ne pas lever une exception et plutôt ne pas autoriser une même dependance deux fois entre la même tâche
-    //Reflechir à tk.dependsOn(tk1, "DD"); et tk.dependsOn(tk1, "FF")
-    //Essayer d'explorer toutes les combinaisons de dependance sur une tache avec d'autres
-    test("throws an exception when a DD dependency is applied between two tasks have this dependency", (assert) => {
+    test("a task must not depend on the same task several times with the same relationship DD", (assert) => {
       let tk = new Task({ startDate: new Date(2026, 0, 2) });
       let tk1 = new Task({ startDate: new Date(2025, 0, 2) });
       tk.dependsOn(tk1, "DD");
-      assert.throws(() => {
-        tk.dependsOn(tk1, "DD");
-      }, new Error("there is already a DD relationship between these two tasks"));
+      tk.dependsOn(tk1, "DD");
+      assert.true(tk.getDependances().length == 1);
     });
 
-    test("throws an exception when a FF dependency is applied between two tasks have this dependency", (assert) => {
+    test("a task must not depend on the same task several times with the same relationship FF", (assert) => {
       let tk = new Task({ dueDate: new Date(2026, 0, 30) });
       let tk1 = new Task({ dueDate: new Date(2026, 0, 25) });
       tk.dependsOn(tk1, "FF");
-      assert.throws(() => {
-        tk.dependsOn(tk1, "FF");
-      }, new Error("there is already a FF relationship between these two tasks"));
+      tk.dependsOn(tk1, "FF");
+      assert.true(tk.getDependances().length == 1);
     });
 
-    test("throws an exception when a FD dependency is applied between two tasks have this dependency", (assert) => {
+    test("a task must not depend on the same task several times with the same relationship FD", (assert) => {
       let tk = new Task({ startDate: new Date(2026, 0, 30) });
       let tk1 = new Task({ dueDate: new Date(2026, 0, 25) });
       tk.dependsOn(tk1, "FD");
-      assert.throws(() => {
-        tk.dependsOn(tk1, "FD");
-      }, new Error("there is already a FD relationship between these two tasks"));
+      tk.dependsOn(tk1, "FD");
+      assert.true(tk.getDependances().length == 1);
     });
   });
 

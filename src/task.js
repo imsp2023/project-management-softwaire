04ec25233d9c36971b26e1277b5ec6740d36f47b
sogan carmen <sogancarmen1@gmail.com>
2023-12-18 07:57:56 +0100

    ajout des titles aux differents tasks dans le fichier de test

--- src/task.js
+++ src/task.js
@@ -13,11 +13,13 @@ class Task {
   constructor(props) {
     if (!props) throw new Error("parameters are required");
     this.#id = props.id;
-    this.#startDate = props.startDate;
-    this.#dueDate = props.dueDate;
+    if (!props.startDate) this.#startDate = new Date();
+    else this.#startDate = props.startDate;
+    if (!props.startDate) this.#startDate = new Date();
+    else this.#dueDate = props.dueDate;
   }
 
-  dependsOn(task, dependanceType, params = "") {
+  dependsOn(task, dependanceType, params = "1") {
     if (task == undefined) throw new Error("missing parameters");
 
     if (!(task instanceof Task))
@@ -26,7 +28,46 @@ class Task {
     if (task == this)
       throw new Error("the task parameter shouldn't same as task depends");
 
-    if (dependanceType == undefined) {
+    if (dependanceType == undefined || dependanceType == "") {
+      if (this.getStartDate() < task.getStartDate()) {
+        this.#startDate = task.getStartDate();
+        // //Ici
+        // if (
+        //   this.#startDate.getDate() != new Date().getDate() &&
+        //   this.#startDate.getMonth() + 1 != new Date().getMonth() + 1 &&
+        //   this.#startDate.getDate() != new Date().getDate()
+        // ) {
+        //   this.#startDate = new Date(
+        //     `${this.#startDate.getFullYear()}-${
+        //       this.#startDate.getMonth() + 1
+        //     }-${this.#startDate.getDate() + parseInt(params) + 1}`
+        //   );
+        // } else {
+        //   this.#startDate = new Date(
+        //     `${this.#startDate.getFullYear()}-${
+        //       this.#startDate.getMonth() + 1
+        //     }-${this.#startDate.getDate() + parseInt(params)}`
+        //   );
+        // }
+      }
+      //Ici
+      // if (
+      //   this.#startDate.getDate() != new Date().getDate() &&
+      //   this.#startDate.getMonth() + 1 != new Date().getMonth() + 1 &&
+      //   this.#startDate.getDate() != new Date().getDate()
+      // ) {
+      //   this.#startDate = new Date(
+      //     `${this.#startDate.getFullYear()}-${this.#startDate.getMonth() + 1}-${
+      //       this.#startDate.getDate() + parseInt(params) + 1
+      //     }`
+      //   );
+      // } else {
+      //   this.#startDate = new Date(
+      //     `${this.#startDate.getFullYear()}-${this.#startDate.getMonth() + 1}-${
+      //       this.#startDate.getDate() + parseInt(params)
+      //     }`
+      //   );
+      // }
       let informationOfDependance = {
         taskId: task.getId(),
         typeOfDependance: "DD",
@@ -41,17 +82,140 @@ class Task {
       )
         throw new Error("parameter dependanceType should be DD or FF or FD");
 
-      // if (typeof params != "string")
-      //   throw new Error("parameter params should be a string");
-
-      if (dependanceType == "DD" && this.getStartDate() < task.getStartDate())
+      if (dependanceType == "DD" && this.getStartDate() < task.getStartDate()) {
         this.#startDate = task.getStartDate();
+        //Ici
+        // if (
+        //   this.#startDate.getDate() != new Date().getDate() &&
+        //   this.#startDate.getMonth() + 1 != new Date().getMonth() + 1 &&
+        //   this.#startDate.getDate() != new Date().getDate()
+        // ) {
+        //   this.#startDate = new Date(
+        //     `${this.#startDate.getFullYear()}-${
+        //       this.#startDate.getMonth() + 1
+        //     }-${this.#startDate.getDate() + parseInt(params) + 1}`
+        //   );
+        // } else {
+        //   this.#startDate = new Date(
+        //     `${this.#startDate.getFullYear()}-${
+        //       this.#startDate.getMonth() + 1
+        //     }-${this.#startDate.getDate() + parseInt(params)}`
+        //   );
+        // }
+      }
+      // else if (
+      //   dependanceType == "DD" &&
+      //   this.getStartDate() >= task.getStartDate()
+      // ) {
+      //   //Ici
+      //   if (
+      //     this.#startDate.getDate() != new Date().getDate() &&
+      //     this.#startDate.getMonth() + 1 != new Date().getMonth() + 1 &&
+      //     this.#startDate.getDate() != new Date().getDate()
+      //   ) {
+      //     this.#startDate = new Date(
+      //       `${this.#startDate.getFullYear()}-${
+      //         this.#startDate.getMonth() + 1
+      //       }-${this.#startDate.getDate() + parseInt(params) + 1}`
+      //     );
+      //   } else {
+      //     this.#startDate = new Date(
+      //       `${this.#startDate.getFullYear()}-${
+      //         this.#startDate.getMonth() + 1
+      //       }-${this.#startDate.getDate() + parseInt(params)}`
+      //     );
+      //   }
+      // }
 
-      if (dependanceType == "FF" && this.getDueDate() < task.getDueDate())
+      if (dependanceType == "FF" && this.getDueDate() < task.getDueDate()) {
         this.#dueDate = task.getDueDate();
+        //Ici
+        // if (
+        //   this.#dueDate.getDate() != new Date().getDate() &&
+        //   this.#dueDate.getMonth() + 1 != new Date().getMonth() + 1 &&
+        //   this.#dueDate.getDate() != new Date().getDate()
+        // ) {
+        //   this.#dueDate = new Date(
+        //     `${this.#dueDate.getFullYear()}-${this.#dueDate.getMonth() + 1}-${
+        //       this.#dueDate.getDate() + parseInt(params) + 1
+        //     }`
+        //   );
+        // } else {
+        //   this.#dueDate = new Date(
+        //     `${this.#dueDate.getFullYear()}-${this.#dueDate.getMonth() + 1}-${
+        //       this.#dueDate.getDate() + parseInt(params)
+        //     }`
+        //   );
+        // }
+      }
+      // else if (
+      //   dependanceType == "FF" &&
+      //   this.getDueDate() >= task.getDueDate()
+      // ) {
+      //   //Ici
+      //   if (
+      //     this.#dueDate.getDate() != new Date().getDate() &&
+      //     this.#dueDate.getMonth() + 1 != new Date().getMonth() + 1 &&
+      //     this.#dueDate.getDate() != new Date().getDate()
+      //   ) {
+      //     this.#dueDate = new Date(
+      //       `${this.#dueDate.getFullYear()}-${this.#dueDate.getMonth() + 1}-${
+      //         this.#dueDate.getDate() + parseInt(params) + 1
+      //       }`
+      //     );
+      //   } else {
+      //     this.#dueDate = new Date(
+      //       `${this.#dueDate.getFullYear()}-${this.#dueDate.getMonth() + 1}-${
+      //         this.#dueDate.getDate() + parseInt(params)
+      //       }`
+      //     );
+      //   }
+      // }
 
-      if (dependanceType == "FD" && this.getStartDate() < task.getDueDate())
+      if (dependanceType == "FD" && this.getStartDate() < task.getDueDate()) {
         this.#startDate = task.getDueDate();
+        //Ici
+        // if (
+        //   this.#startDate.getDate() != new Date().getDate() &&
+        //   this.#startDate.getMonth() + 1 != new Date().getMonth() + 1 &&
+        //   this.#startDate.getDate() != new Date().getDate()
+        // ) {
+        //   this.#startDate = new Date(
+        //     `${this.#startDate.getFullYear()}-${
+        //       this.#startDate.getMonth() + 1
+        //     }-${this.#startDate.getDate() + parseInt(params) + 1}`
+        //   );
+        // } else {
+        //   this.#startDate = new Date(
+        //     `${this.#startDate.getFullYear()}-${
+        //       this.#startDate.getMonth() + 1
+        //     }-${this.#startDate.getDate() + parseInt(params)}`
+        //   );
+        // }
+      }
+      // else if (
+      //   dependanceType == "FD" &&
+      //   this.getStartDate() >= task.getDueDate()
+      // ) {
+      //   //Ici
+      //   if (
+      //     this.#startDate.getDate() != new Date().getDate() &&
+      //     this.#startDate.getMonth() + 1 != new Date().getMonth() + 1 &&
+      //     this.#startDate.getDate() != new Date().getDate()
+      //   ) {
+      //     this.#startDate = new Date(
+      //       `${this.#startDate.getFullYear()}-${
+      //         this.#startDate.getMonth() + 1
+      //       }-${this.#startDate.getDate() + parseInt(params) + 1}`
+      //     );
+      //   } else {
+      //     this.#startDate = new Date(
+      //       `${this.#startDate.getFullYear()}-${
+      //         this.#startDate.getMonth() + 1
+      //       }-${this.#startDate.getDate() + parseInt(params)}`
+      //     );
+      //   }
+      // }
       let informationOfDependance = {
         taskId: task.getId(),
         typeOfDependance: dependanceType,
@@ -118,6 +282,10 @@ class Task {
     return this.#startDate;
   }
 
+  setStartDate(startDate) {
+    this.#startDate = startDate;
+  }
+
   getDueDate() {
     return this.#dueDate;
   }
@@ -134,3 +302,24 @@ class Task {
     return this.#taskResponsible;
   }
 }
+
+// let tk = new Task({ startDate: new Date(2020, 11, 1) });
+// let tk1 = new Task({ startDate: new Date(2020, 2, 1) });
+// tk1.dependsOn(tk, null, "2");
+// console.log(tk1.getStartDate());
+
+// let tk = new Task({});
+// let tk1 = new Task({});
+// tk1.dependsOn(tk, "FF", "2");
+// console.log(tk1.getDueDate());
+
+// let params = "2";
+// let paramString = "Salut";
+// const lt = new Date("2020-08-1");
+// console.log(lt);
+// let titi = lt.getDate() + parseInt(params);
+// console.log(typeof titi);
+// const tlEnfant = new Date(
+//   `${lt.getFullYear()}-${lt.getMonth() + 1}-${titi + 1}`
+// );
+// console.log(tlEnfant);

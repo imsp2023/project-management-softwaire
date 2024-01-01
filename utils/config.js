234769be4c1ae0eaacec79e7dad24c15c3a8b625
Duamelo <david.dosseh@imsp-uac.org>
2024-01-01 19:18:01 +0100

    edit config

--- utils/config.js
+++ utils/config.js
@@ -1,3 +1,8 @@
 let config = {
-
+    priority: ['basse', 'normale', 'élevée', 'urgente'],
+    status: ['à faire', 'en cours', 'achevé'],
+    loadData: ()=>{},
+    saveTask: (task)=>{},
+    saveProject: (project)=>{},
+    saveMember: (member)=>{}
 };
